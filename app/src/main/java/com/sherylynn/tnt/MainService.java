7b1235505086d8051a1054c843e2e7f142e340d8
sherylynn <352281674@qq.com>
2019-12-11 18:44:55 +0800

    add service

---
+++ app/src/main/java/com/sherylynn/tnt/MainService.java
@@ -0,0 +1,16 @@
+package com.sherylynn.tnt;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class MainService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
